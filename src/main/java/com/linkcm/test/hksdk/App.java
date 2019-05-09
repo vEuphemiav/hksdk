eb4628d2837d19e1d8a886a8e612b536d74cd114
yuanfeizheng <yuanfeizheng@linkcm.com>
2019-05-09 16:18:25 +0800

    first commit

---
+++ src/main/java/com/linkcm/test/hksdk/App.java
@@ -0,0 +1,13 @@
+package com.linkcm.test.hksdk;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
